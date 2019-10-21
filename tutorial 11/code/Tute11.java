aa4df65f7765904fa54c85816c9a5c75879d580e
Chuang Wang <chuangw@student.unimelb.edu.au>
2019-10-21 11:44:15 +1100

    update tute11 code & slides

--- tutorial 11/code/Tute11.java
+++ tutorial 11/code/Tute11.java
@@ -12,6 +12,7 @@ import java.util.Scanner;
 public class Tute11 {
     public static void main(String[] args) {
         List<String> friends = new ArrayList<>();
+
         Scanner keyboard = new Scanner(System.in);
         System.out.println("Which action would you like to perform?");
         System.out.println("Type E for enter names");
@@ -23,17 +24,18 @@ public class Tute11 {
     }
 
     private static void getOption(Scanner keyboard, List<String> friends) {
-        String s = keyboard.nextLine();
 
-        while (!s.equalsIgnoreCase("q")){
+        while (true){
+            String s = keyboard.nextLine();
             if (s.equalsIgnoreCase("e")) {
                 addNames(keyboard, friends);
             } else if (s.equalsIgnoreCase("d")) {
                 deleteName(keyboard, friends);
+            } else if (s.equalsIgnoreCase("q")) {
+                System.exit(0);
             } else {
                 System.out.println("Incorrect command, please try again");
             }
-            s = keyboard.nextLine();
         }
     }
 
@@ -51,8 +53,6 @@ public class Tute11 {
         }
 
         System.out.println("next action to perform");
-
-        getOption(keyboard, friends);
     }
 
     private static void deleteName(Scanner keyboard, List<String> friends) {
@@ -68,6 +68,7 @@ public class Tute11 {
             try {
                 i = keyboard.nextInt();
                 friends.remove(i);
+                System.out.println("Removed successfully");
             } catch (InputMismatchException e) {
                 System.out.println("Sorry, please type an integer");
                 i = keyboard.nextInt();
@@ -75,10 +76,10 @@ public class Tute11 {
                 System.out.println("Sorry, this index is out of bounds");
             }
         }
+        // remove the rest of the line; otherwise, "" will remain in the keyboard
+        keyboard.nextLine();
 
         System.out.println("next action to perform");
-
-        getOption(keyboard, friends);
     }
 
 }
